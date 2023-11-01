9f6ac04452f14d404a7bc106964f5ab43f9ae348
Oscar Rodriguez Villalobos <lagartoverde97@gmail.com>
2023-11-01 12:30:06 +0100

    fix bug with sparql query

--- utils/commonQueries.js
+++ utils/commonQueries.js
@@ -58,23 +58,24 @@ export async function loadSiteTypes(siteId) {
   const adminUnitClassification = await getAdminUnitClassification(siteId);
   let filteredTypes = [];
 
-  filteredTypes.push('f1381723dec42c0b6ba6492e41d6f5dd'); // Maatschappelijke zetel
+  filteredTypes.push('"f1381723dec42c0b6ba6492e41d6f5dd"'); // Maatschappelijke zetel
 
   if (isWorshipAdministrativeUnit(adminUnitClassification)) {
-    filteredTypes.push('dd0418307e7038c0c3809e3ec03a0932'); // Hoofdgebouw erediensten
+    filteredTypes.push('"dd0418307e7038c0c3809e3ec03a0932"'); // Hoofdgebouw erediensten
   } else if (isMunicipality(adminUnitClassification)) {
-    filteredTypes.push('57e8e5498ca84056b8a87631a26c90af'); // Gemeentehuis
-    filteredTypes.push('fbec5e94aba343b0a7361aca8a0c7d79'); // Ander administratief adres
+    filteredTypes.push('"57e8e5498ca84056b8a87631a26c90af"'); // Gemeentehuis
+    filteredTypes.push('"fbec5e94aba343b0a7361aca8a0c7d79"'); // Ander administratief adres
   } else if (isAgbApbOrIGS(adminUnitClassification)) {
-    filteredTypes.push('dcc01338-842c-4fbd-ba68-3ca6f3af975c'); // CorrespondentieAddres
+    filteredTypes.push('"dcc01338-842c-4fbd-ba68-3ca6f3af975c"'); // CorrespondentieAddres
   } else if (isProvince(adminUnitClassification)) {
-    filteredTypes.push('15f2683c61b74541b27b64b4365806c7'); // Provinciehuis
+    filteredTypes.push('"15f2683c61b74541b27b64b4365806c7"'); // Provinciehuis
   } else if (isDistrict(adminUnitClassification)) {
-    filteredTypes.push('db13a289b78e42d19d8d1d269b61b18f'); // Districtshuis
+    filteredTypes.push('"db13a289b78e42d19d8d1d269b61b18f"'); // Districtshuis
   }
 
   const types = queryDatabaseTypes(filteredTypes);
   const metaTriples = generateMetaTriplesForSiteTypes(types);
+  return metaTriples;
 }
 
 async function getAdminUnitClassification(siteId) {
